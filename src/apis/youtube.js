d383c2ff341f6a0d83daef515a41fb3a32e782a5
Thomas Brouillet <thomasbrouillet@gmail.com>
2020-10-27 17:35:12 +0100

    final

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import KEY from '../env';
+
+const KEY = 'AIzaSyDzbh_nI0EATAP6MrcGU-Fkm-xJEmA27ag';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
--- src/env.js
+++
@@ -1,5 +0,0 @@
-const KEY = 'AIzaSyDzbh_nI0EATAP6MrcGU-Fkm-xJEmA27ag';
-
-export default KEY;
-
-//fichier cache
